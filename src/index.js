0a33c0e4bef24645965a136e7868e32d0190c4b5
Victor Hripko <hripko.v@soft2bet.com>
2020-08-26 16:43:35 +0300

    add lodash throttle

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import './style.scss';
+import { throttle } from 'lodash-es';
 
 (() => {
   const block = document.querySelector('.block');
 
-  document.addEventListener('mousemove', (evt) => {
+  const mouseMove = (e) => {
     const parentCenterY = window.innerHeight / 2;
     const parentCenterX = window.innerWidth / 2;
 
-    const rotateX = (parentCenterX - evt.clientX) / 100;
-    const rotateY = (parentCenterY - evt.clientY) / 100;
+    const rotateX = (parentCenterX - e.clientX) / 100;
+    const rotateY = (parentCenterY - e.clientY) / 100;
 
     block.style.transform = `rotateX(${rotateY}deg) rotateY(${-rotateX}deg)`;
-  });
+  };
+
+  const throttleMove = throttle(mouseMove, 10);
+  document.addEventListener('mousemove', throttleMove);
 })();
